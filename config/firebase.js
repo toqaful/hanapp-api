ba3a0756c9e8c6717c1f7e921c59dfcadc779b48
Toqaful Yaosaful Herrera <herreratoqafulyaosaful@gmail.com>
2025-07-22 14:02:50 +0000

    dynamic config

--- config/firebase.js
+++ config/firebase.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+// Step 1: Decode the env variable and write the JSON file
+const serviceAccountPath = path.join('serviceAccountKey.json');
+
+if (!fs.existsSync(serviceAccountPath)) {
+  const jsonStr = Buffer.from(process.env.SERVICE_ACCOUNT_JSON, 'base64').toString('utf-8');
+  fs.writeFileSync(serviceAccountPath, jsonStr);
+  console.log('serviceAccountKey.json recreated');
+}
+
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
 
